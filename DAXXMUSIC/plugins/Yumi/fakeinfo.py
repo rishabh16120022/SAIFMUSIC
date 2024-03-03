76247d1685b123e3fd52cf1a7f8a44c58e2c9a2e
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-03 23:46:24 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -1,7 +1,7 @@
 """***
 MIT License
 
-Copyright (c) [2023] [DAXX TEAM]
+Copyright (c) [2023] [SAIFDEAD]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -50,6 +50,6 @@ def generate_fake_user_by_country(client, message):
         street = user_info['location']['street']['name']
         zip_code = user_info['location']['postcode']
         # Reply with the generated fake user information for the specified country
-        message.reply_text(f"𝗡𝗔𝗠𝗘➪ {first_name} {last_name}\n\n𝗘𝗠𝗔𝗜𝗟➪ {email}\n\n𝗖𝗢𝗨𝗡𝗧𝗥𝗬➪ {country}\n\n𝗦𝗧𝗔𝗧𝗘➪ {state}\n\n𝗖𝗜𝗧𝗬: {city}\n\n𝗔𝗗𝗗𝗥𝗘𝗦𝗦:➪{street}\n\n𝗭𝗜𝗣 𝗖𝗢𝗗𝗘➪ {zip_code}")
+        message.reply_text(f"𝗡ᴀᴍᴇ➪ {first_name} {last_name}\n\n𝗘ᴍᴀɪʟ➪ {email}\n\n𝗖ᴏᴜɴᴛʀʏ➪ {country}\n\n𝗦ᴛᴀᴛᴇ➪ {state}\n\n𝗖ɪᴛʏ: {city}\n\n𝐀ᴅᴅʀᴇss:➪{street}\n\n𝗭ɪᴘ ᴄᴏᴅᴇ➪ {zip_code}")
     else:
-        message.reply_text(f"Failed to generate fake user information for {country_name}.")
+        message.reply_text(f"ғᴀɪʟᴇᴅ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ғᴀᴋᴇ ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғᴏʀ {country_name}.")
