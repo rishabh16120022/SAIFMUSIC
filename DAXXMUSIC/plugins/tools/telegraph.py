660cd7e3829f4eeea22bce041d25019cc2a80eba
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-04 03:41:12 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -8,7 +8,7 @@ from pyrogram.types import InputMediaPhoto
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        i = message.reply("𝐌ᴀᴋᴇɪɴɢ 𝐀 𝐋ɪɴᴋ...")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
@@ -22,7 +22,7 @@ def ul(_, message):
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("𝐌𝙰𝙺𝙴 𝐀 𝐋𝙸𝙽𝙺...")
+        i = message.reply("𝐌𝙰𝙺𝙴ɪɴɢ 𝐀 𝐋𝙸𝙽𝙺...")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
