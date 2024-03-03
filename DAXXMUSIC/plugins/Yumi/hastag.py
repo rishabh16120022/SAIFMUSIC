3996f73609535808e067321179baaa929a227e01
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-03 23:23:57 +0530

    Update hastag.py

--- DAXXMUSIC/plugins/Yumi/hastag.py
+++ DAXXMUSIC/plugins/Yumi/hastag.py
@@ -15,7 +15,7 @@ async def hastag(bot, message):
 
         content = BSP(res, 'html.parser').find("div", {"class":"copy-hashtags"}).string
     except IndexError:
-        return await message.reply_text("Example:\n\n/hastag python")
+        return await message.reply_text("Example:\n\n/ʜᴀsᴛᴀɢ sᴀɪғ")
         
     
     await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{content}</pre>", quote=True)
