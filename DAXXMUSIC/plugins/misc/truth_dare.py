4c3a8a4a0e8be6638019bf26aed39c456c9638b0
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-05 01:10:29 +0530

    Update truth_dare.py

--- DAXXMUSIC/plugins/misc/truth_dare.py
+++ DAXXMUSIC/plugins/misc/truth_dare.py
@@ -15,11 +15,11 @@ def get_truth(client, message):
         response = requests.get(truth_api_url)
         if response.status_code == 200:
             truth_question = response.json()["question"]
-            message.reply_text(f"Truth question:\n\n{truth_question}")
+            message.reply_text(f"ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ:\n\n{truth_question}")
         else:
-            message.reply_text("Failed to fetch a truth question. Please try again later.")
+            message.reply_text("ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀ ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
     except Exception as e:
-        message.reply_text("An error occurred while fetching a truth question. Please try again later.")
+        message.reply_text("ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ғᴇᴛᴄʜɪɴɢ a ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
 @app.on_message(filters.command("dare"))
 def get_dare(client, message):
@@ -28,8 +28,8 @@ def get_dare(client, message):
         response = requests.get(dare_api_url)
         if response.status_code == 200:
             dare_question = response.json()["question"]
-            message.reply_text(f"Dare question:\n\n{dare_question}")
+            message.reply_text(f"ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ:\n\n{dare_question}")
         else:
-            message.reply_text("Failed to fetch a dare question. Please try again later.")
+            message.reply_text("ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀ ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
     except Exception as e:
-        message.reply_text("An error occurred while fetching a dare question. Please try again later.")
+        message.reply_text("ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ғᴇᴛᴄʜɪɴɢ ᴀ ᴅᴀʀᴇ ǫᴜᴇsᴛɪᴏɴ. Please try again later.")
