8c16e13bdb6c5969357ed5ce9a402f614883d54d
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-05 00:25:29 +0530

    Update gayfun.py

--- DAXXMUSIC/plugins/misc/gayfun.py
+++ DAXXMUSIC/plugins/misc/gayfun.py
@@ -15,11 +15,11 @@ def calculate_gay_percentage():
 def generate_gay_response(gay_percentage):
     # Define random texts and emojis for different gay percentage ranges
     if gay_percentage < 30:
-        return "You're straight as an arrow. 🏳️‍🌈"
+        return "ʏᴏᴜ'ʀᴇ sᴛʀᴀɪɢʜᴛ ᴀs ᴀɴ ᴀʀʀᴏᴡ. 🏳️‍🌈"
     elif 30 <= gay_percentage < 70:
         return "You might have a bit of a rainbow in you. 🌈"
     else:
-        return "You're shining with rainbow colors! 🌟🏳️‍🌈"
+        return "ʏᴏᴜ'ʀᴇ sʜɪɴɪɴɢ ᴡɪᴛʜ ʀᴀɪɴʙᴏᴡ ᴄᴏʟᴏʀs! 🌟🏳️‍🌈"
 
 @app.on_message(filters.command("gay") & filters.regex(r'^/gay$'))
 def gay_calculator_command(client, message: Message):
