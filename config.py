d4fd8b4703a9859494e6cffdf0820bcb35a4855a
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-13 19:25:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SAIFDEAD/MUSIC1",
+    "https://github.com/SAIFDEAD/SAIFMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
