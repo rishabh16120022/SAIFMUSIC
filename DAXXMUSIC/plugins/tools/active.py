627306da1846f3e9af88bc78f141ec27c5810ae6
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-04-21 17:56:39 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- DAXXMUSIC/plugins/tools/active.py
+++ DAXXMUSIC/plugins/tools/active.py
@@ -79,4 +79,4 @@ async def activevi_(_, message: Message):
 async def start(client: Client, message: Message):
     ac_audio = str(len(await get_active_chats()))
     ac_video = str(len(await get_active_video_chats()))
-    await message.reply_text(f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('✯ ᴄʟᴏsᴇ ✯', callback_data=f"close")]]))
+    await message.reply_text(f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('✯ 𝐂ʟᴏsᴇ ✯', callback_data=f"close")]]))
