5f0afbcd79d6fb3248463520231ce20e2b527fa1
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-05-30 23:28:40 +0530

    play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -90,11 +90,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"𝐘𝐚𝐬𝐡𝐢𝐤𝐚𝐌𝐮𝐬𝐢𝐜𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"𝐘𝐚𝐬𝐡𝐢𝐤𝐚𝐌𝐮𝐬𝐢𝐜𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"DAXXPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
