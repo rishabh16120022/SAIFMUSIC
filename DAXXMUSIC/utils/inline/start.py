c2027c9b1bd0cf1524a237f4f08b3872a7a1df4d
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-05-30 23:50:40 +0530

    start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -29,7 +29,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton("🥀 ʏᴀʜᴀ ᴀᴀᴏ ✨", url=f"https://t.me/pandazzzworld"),
         ],
     ]
     return buttons
